48b7a0410fafe6a9a7ae301b525b5c36cedbe766
mareto5555 <77633933+mareto5555@users.noreply.github.com>
2021-01-26 18:53:59 +0100

    Initial commit

---
+++ src/components/Podcast.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="site-wrap" v-if="podcast"></div>
+</template>  
+
+
+<script>
+import PodcastsFooter from "./PodcastsFooter";
+import PodcastsHeader from "./PodcastsHeader";
+import {mapState, mapActions } from "vuex";
+
+export default {
+  name: "Podcast",
+  components: { PodcastsFooter, PodcastsHeader },
+  
+created() {
+    this.loadPodcast(this.$route.params.id);
+},
+computed:{...mapState(['podcast'])},
+methods: {
+        ...mapActions(['loadPodcast'])
+},
+};
+</script>
